f710bcf761ba60d55c159491ac8d93633080b517
nathan-popcore <nathaniel@popcore.com>
2021-01-25 16:14:19 +0100

    Changes to be committed: 	modified:   macinfo.sh

--- macinfo.sh
+++ macinfo.sh
@@ -6,4 +6,10 @@ echo "$HOSTNAME" >> $file
 echo "$USER" >> $file
 fdesetup status >> $file
 system_profiler SPHardwareDataType | egrep "Serial Number|Model|Memory|CPU|Processor Name|Speed" |sort |sed 's/      //g' >> $file
+curl -s https://support-sp.apple.com/sp/product?cc=$(
+  system_profiler SPHardwareDataType \
+        | awk '/Serial/ {print $4}' \
+            | cut -c 9-
+  
+  ) | sed 's|.*<configCode>\(.*\)</configCode>.*|\1|' >> $file
 
